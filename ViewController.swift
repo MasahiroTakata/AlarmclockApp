9a8e83f7cd95b58cd7c33c5d19f9af38ea0fa470
高田将弘 <takata005581016@gmail.com>
2019-08-03 23:52:18 +0900

    recommit

--- ViewController.swift
+++ ViewController.swift
@@ -7,16 +7,25 @@
 //
 
 import UIKit
+import UserNotifications // 通知の為のフレームワーク
 
 class ViewController: UIViewController {
-
-
     @IBOutlet weak var alarmTime: UITextField!
     @IBOutlet weak var alarmMusic: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        // 目覚まし時計のアプリ
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) {
+            (granted, error) in
+            if granted {
+                print("通知許可")
+                let center = UNUserNotificationCenter.current()
+                center.delegate = self as? UNUserNotificationCenterDelegate
+            } else {
+                print("通知拒否")
+            }
+        }
     }
     // 設定情報を保存するメソッド
     @IBAction func saveInformation(_ sender: Any) {
